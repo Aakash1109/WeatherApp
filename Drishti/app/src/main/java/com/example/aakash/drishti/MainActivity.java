e55a390347213fe87e2bd48d828bd7d84e326822
Aakash1109 <akbirari119@gmail.com>
2018-12-26 20:25:34 +0530

    slides added content to add

--- Drishti/app/src/main/java/com/example/aakash/drishti/MainActivity.java
+++ Drishti/app/src/main/java/com/example/aakash/drishti/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aakash.drishti;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Intent intent = new Intent(MainActivity.this,SlidingInfo.class);
+        startActivity(intent);
     }
 }
