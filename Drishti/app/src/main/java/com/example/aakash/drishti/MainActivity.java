9a6e2006d4c4d143c938cf25b34c5849fb373046
deepa0597 <deepa0597@gmail.com>
2018-12-30 12:25:06 +0530

    View Pager Sliders added with 8 buttons

--- Drishti/app/src/main/java/com/example/aakash/drishti/MainActivity.java
+++ Drishti/app/src/main/java/com/example/aakash/drishti/MainActivity.java
@@ -11,8 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Intent intent = new Intent(MainActivity.this,SlidingInfo.class);
-        startActivity(intent);
     }
 }
