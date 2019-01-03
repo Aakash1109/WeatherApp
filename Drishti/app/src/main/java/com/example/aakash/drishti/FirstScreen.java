f046b3a69ec7df549071756afc75f0be5c0b8243
deepa0597 <deepa0597@gmail.com>
2019-01-03 22:17:04 +0530

    up to date fragment,loc,datetime

---
+++ Drishti/app/src/main/java/com/example/aakash/drishti/FirstScreen.java
@@ -0,0 +1,69 @@
+package com.example.aakash.drishti;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class FirstScreen extends Fragment implements View.OnClickListener {
+    private View mView;
+    Button button1, button2;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FirstScreen() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView= inflater.inflate(R.layout.fragment_first_screen, container, false);
+        button1=(Button)mView.findViewById(R.id.one);
+        button1.setOnClickListener(this);
+        button2=(Button)mView.findViewById(R.id.two);
+        button2.setOnClickListener(this);
+        return mView;
+    }
+
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.one:
+                Intent intent = new Intent(getActivity(),LocationTracker.class);
+                startActivity(intent);
+                break;
+            case R.id.two:
+                Intent intent1 = new Intent(getActivity(),CurrentDateTime.class);
+                startActivity(intent1);
+                break;
+        }
+    }
+}
